7cf20d48235c6bd03c86415cf1baa7e51584ff88
Lwazi Ncube <lwazincubex@gmail.com>
2024-08-21 23:37:16 +0200

    .

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Hello world
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
